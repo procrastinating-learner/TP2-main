08342185487e4589a67941d3976f2291b535e9ce
procrastinating-learner <yasserrabai@gmail.com>
2025-11-07 16:36:59 +0100

    Initial commit

---
+++ src/main/java/com/example/dao/DaoImpl.java
@@ -0,0 +1,17 @@
+package com.example.dao;
+
+
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+@Profile("prod")  // Ce bean sera actif uniquement avec le profil "prod"
+public class DaoImpl implements IDao {
+    @Override
+    public double getValue() {
+        System.out.println("Version de production");
+        return 100.0;
+    }
+}
